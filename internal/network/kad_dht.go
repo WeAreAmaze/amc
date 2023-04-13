6ea1864eb298a3227a3028b5242ba21a88ccd2ec
qifanwang <>
2023-04-13 11:33:31 +0800

    modified

--- internal/network/kad_dht.go
+++ internal/network/kad_dht.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the AmazeChain library. If not, see <http://www.gnu.org/licenses/>.
 // 213333333
-// 123111121122
+// 12311112112233
 package network
 
 import (

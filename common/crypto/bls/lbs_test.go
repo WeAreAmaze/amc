9d3b4057a5613c91a40cec4538cb294de9ed5e30
gjh <1430217064@qq.com>
2023-04-14 15:56:12 +0800

    添加注释

--- common/crypto/bls/lbs_test.go
+++ common/crypto/bls/lbs_test.go
@@ -40,6 +40,7 @@ func TestAggregateVerify(t *testing.T) {
 	assert.Equal(t, true, aggSig.AggregateVerify(pubkeys, msgs), "Signature did not verify")
 }
 
+// Compress aggregated signatures through Marshal
 func TestAggregateVerify_CompressedSignatures(t *testing.T) {
 	pubkeys := make([]common.PublicKey, 0, 100)
 	sigs := make([]common.Signature, 0, 100)
@@ -58,7 +59,7 @@ func TestAggregateVerify_CompressedSignatures(t *testing.T) {
 	}
 	aggSig := AggregateSignatures(sigs)
 	assert.Equal(t, true, aggSig.AggregateVerify(pubkeys, msgs), "Signature did not verify")
-
+	//Use SigBytes to compress the Signatures
 	aggSig2, err := AggregateCompressedSignatures(sigBytes)
 	assert.NoError(t, err)
 	assert.Equal(t, aggSig.Marshal(), aggSig2.Marshal(), "Signature did not match up")

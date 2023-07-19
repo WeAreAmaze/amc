7684dbc7b92423af502e58909ebb1a8b904c1823
jack meng <mengweifu1985@gmail.com>
2023-07-19 16:41:32 +0800

    fixed bugs

--- internal/sync/initial-sync/round_robin.go
+++ internal/sync/initial-sync/round_robin.go
@@ -61,7 +61,7 @@ func (s *Service) syncToFinalizedBlockNr(ctx context.Context, highestExpectedBlo
 		s.processFetchedData(ctx, s.cfg.Chain.CurrentBlock().Number64(), data)
 	}
 
-	log.Info("Synced to finalized epoch - now syncing blocks up to current head", "syncedBlockNr", s.cfg.Chain.CurrentBlock().Number64(), "highestExpectedBlockNr", highestExpectedBlockNr)
+	log.Info("Synced to finalized epoch - now syncing blocks up to current head", "syncedBlockNr", s.cfg.Chain.CurrentBlock().Number64().Uint64(), "highestExpectedBlockNr", highestExpectedBlockNr.Uint64())
 	if err := queue.stop(); err != nil {
 		log.Debug("Error stopping queue", "err", err)
 	}
